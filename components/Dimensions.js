42509f08792928f0a976aa1f37c1496d934595cb
Erasedon <46790756+Erasedon@users.noreply.github.com>
2023-08-03 10:52:09 +0200

    probleme de taille pas fini (file dimensions)

--- components/Dimensions.js
+++ components/Dimensions.js
@@ -4,25 +4,5 @@ import {View, StyleSheet, Text, Dimensions} from 'react-native';
   // const windowHeight = Dimensions.get('window').height;
   const screenHeight = Dimensions.get('screen').height;
   const screenWidth = Dimensions.get('screen').width;
-// const Dimscreens = () =>{
 
-    // const [dimensions, setDimensions] = useState({
-    //   // windowW: windowWidth,
-    //   // windowH: windowHeight,
-    //   screenW: screenWidth,
-    //   screenH: screenHeight,
-    // });
-  
-    // useEffect(() => {
-    //   const subscription = Dimensions.addEventListener(
-    //     'change',
-    //     // ({windowW, windowH,screenW,screenH}) => {
-    //     ({screenW,screenH}) => {
-    //       // setDimensions({windowW, windowH,screenW,screenH});
-    //       setDimensions({screenW,screenH});
-    //     },
-    //   );
-    //   return () => subscription?.remove();
-    // });
-// }
-export default screenHeight;
+export default screenWidth|screenHeight;
