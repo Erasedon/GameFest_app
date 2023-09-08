033b50bd5efac2d9b0e134b04d50a2430e10b779
Erasedon <46790756+Erasedon@users.noreply.github.com>
2023-09-08 12:06:40 +0200

    nouvelle fonctialité carousel

---
+++ components/ActiviteListComponent.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from "react";
+import { View, Text,Image } from "react-native";
+import axios from "axios";
+import {StyleCMZ,StyleNancy, StyleStrasbourg} from "./Styles/StyleStand";
